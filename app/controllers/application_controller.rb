e06f9fde7f2093b82df5a96d6016a96acafd1279
John Travers <johnk.travers@gmail.com>
2019-11-19 14:04:46 -0700

    Add render_show_error and error_code methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+
+  def render_show_error(resource)
+    render json: {
+      errors: [{
+        code: error_code(resource),
+        title: "#{resource} with given ID does not exist."
+      }]
+    }
+  end
+
+  def error_code(resource)
+    if resource == 'Merchant'
+      '3000'
+    end
+  end
+
 end
