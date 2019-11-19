941bc7f6f9d220a94fdd6dfb2c5db138ce43092e
John Travers <johnk.travers@gmail.com>
2019-11-19 13:15:10 -0700

    Add test for merchant show api endpoint

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe 'Merchants API' do
     expect(merchants['data'].length).to eq(4)
   end
 
+  it 'show page by merchant id' do
+    id = create(:merchant).id
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant['data']['id'].to_i).to eq(id)
+  end
+
 end
