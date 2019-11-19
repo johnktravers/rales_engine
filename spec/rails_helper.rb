b80c6e87d129a6b5feda0082e60e953a7e35dcf2
John Travers <johnk.travers@gmail.com>
2019-11-18 17:12:52 -0700

    Add simplecov to rails helper file to monitor test coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -60,6 +63,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  
+
   config.include FactoryBot::Syntax::Methods
 end
