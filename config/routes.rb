64f5fb81d3f554bfe80e0ad77b73d2c928c474bd
John Travers <johnk.travers@gmail.com>
2019-11-19 21:25:21 -0700

    Add merchant relationship routes for item and invoice endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
-        get '/find',     to: 'search#show'
-        get '/find_all', to: 'search#index'
-        get '/random',   to: 'random#show'
+        get '/find',         to: 'search#show'
+        get '/find_all',     to: 'search#index'
+        get '/random',       to: 'random#show'
+        get '/:id/items',    to: 'items#index'
+        get '/:id/invoices', to: 'invoices#index'
       end
       resources :merchants, only: [:index, :show]
     end
