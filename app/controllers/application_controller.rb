c7eb4857963d61cffac4bd1471cd05e7c45d7f94
John Travers <johnk.travers@gmail.com>
2019-11-19 18:32:38 -0700

    Add render_find_error method for find endpoint error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,15 @@ class ApplicationController < ActionController::API
     }
   end
 
+  def render_find_error(resource)
+    render json: {
+      errors: [{
+        code: error_code(resource),
+        title: "#{resource} with given attributes does not exist."
+      }]
+    }
+  end
+
   def error_code(resource)
     if resource == 'Merchant'
       '3000'
