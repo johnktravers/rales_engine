6fdd1ec630639593e2a8f9fb5fa5466f64609961
John Travers <johnk.travers@gmail.com>
2019-11-21 09:53:19 -0700

    Add invoices search controller with index and show actions

---
+++ app/controllers/api/v1/invoices/search_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::Invoices::SearchController < ApplicationController
+
+  def show
+    invoices = Invoice.where(filter_params)
+
+    if invoices.any?
+      serialized_invoice = InvoiceSerializer.new(invoices.first)
+      render json: serialized_invoice
+    else
+      render_find_error('Invoice')
+    end
+  end
+
+  def index
+    invoices = Invoice.where(filter_params)
+
+    if invoices.any?
+      serialized_invoices = InvoiceSerializer.new(invoices)
+      render json: serialized_invoices
+    else
+      render_find_all_error('Invoice')
+    end
+  end
+
+
+  private
+
+  def filter_params
+    params.permit(:id, :status, :merchant_id, :customer_id, :created_at, :updated_at)
+  end
+
+end
