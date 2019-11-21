99f23ceaf52ea4c0031f4885287f13e52016a9d4
John Travers <johnk.travers@gmail.com>
2019-11-21 13:14:51 -0700

    Add many to many relationship between invoices and items

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -9,8 +9,9 @@ RSpec.describe Invoice, type: :model do
   describe 'relationships' do
     it { should belong_to :customer }
     it { should belong_to :merchant }
-    it { should have_many :invoice_items }
     it { should have_many :transactions }
+    it { should have_many :invoice_items }
+    it { should have_many(:items).through(:invoice_items) }
   end
 
   describe 'status' do
