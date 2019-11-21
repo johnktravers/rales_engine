24e265d32736381d0e4acf0b15b4edd1addbad13
John Travers <johnk.travers@gmail.com>
2019-11-20 22:49:55 -0700

    Add associated resources to invoice and item factories

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :invoice do
     status { 0 }
+    customer
+    merchant
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { Faker::Commerce.product_name }
     description { Faker::TvShows::GameOfThrones.quote }
     unit_price { Faker::Number.decimal(l_digits: 2) }
+    merchant
   end
 end
