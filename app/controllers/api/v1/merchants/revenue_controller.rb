9579050b8e0cae585d072ed91f3f9a5f8c5fb63d
John Travers <johnk.travers@gmail.com>
2019-11-20 23:08:25 -0700

    Add show action to merchants revenue controller to show revenue on a date

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -10,4 +10,24 @@ class Api::V1::Merchants::RevenueController < ApplicationController
     end
   end
 
+  def show
+    if params[:date] && validate_date(params[:date])
+      revenue = Invoice.total_revenue_on_date(params[:date])
+      render json: { data: {
+        id: params[:date].gsub(/-/, ''),
+        type: 'revenue',
+        attributes: { total_revenue: '%.2f' % revenue }
+      } }
+    else
+      render_error('Incorrect date format. Please use format YYYY-MM-DD.')
+    end
+  end
+
+
+  private
+
+  def validate_date(date)
+    (DateTime.parse(date) rescue ArgumentError) != ArgumentError
+  end
+
 end
