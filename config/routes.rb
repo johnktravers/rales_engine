4d438851c514aa506651bcfb4b89eb206bdfabf2
John Travers <johnk.travers@gmail.com>
2019-11-20 23:10:05 -0700

    Add route for endpoint that shows a merchant's favorite customer

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
         get '/:id/invoices',          to: 'invoices#index'
         get '/most_revenue',          to: 'revenue#index'
         get '/revenue',               to: 'revenue#show'
+        get '/:id/favorite_customer', to: 'customers#show'
       end
       resources :merchants, only: [:index, :show]
     end
