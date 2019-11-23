42332a64255e88719e112fa5af58c493e5add9d3
John Travers <johnk.travers@gmail.com>
2019-11-23 15:23:32 -0700

    Add routes for items index, show, find, find all, and random endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -32,9 +32,16 @@ Rails.application.routes.draw do
         get '/:id/invoice', to: 'invoices#show'
       end
 
+      namespace :items do
+        get '/find',     to: 'search#show'
+        get '/find_all', to: 'search#index'
+        get '/random',   to: 'random#show'
+      end
+
       resources :merchants,     only: [:index, :show]
       resources :invoices,      only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
+      resources :items,         only: [:index, :show]
     end
   end
 
