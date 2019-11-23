f64c4319e528f0f6a56e2866da9e6d1283c06db8
John Travers <johnk.travers@gmail.com>
2019-11-23 13:48:32 -0700

    Add invoice items search controller with index and show actions

---
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::InvoiceItems::SearchController < ApplicationController
+
+  def show
+    invoice_items = InvoiceItem.where(filter_params)
+
+    if invoice_items.any?
+      serialized_invoice_item = InvoiceItemSerializer.new(invoice_items.first)
+      render json: serialized_invoice_item
+    else
+      render_find_error('Invoice item')
+    end
+  end
+
+  def index
+    invoice_items = InvoiceItem.where(filter_params)
+
+    if invoice_items.any?
+      serialized_invoice_items = InvoiceItemSerializer.new(invoice_items)
+      render json: serialized_invoice_items
+    else
+      render_find_all_error('Invoice item')
+    end
+  end
+
+
+  private
+
+  def filter_params
+    params.permit(
+      :id,
+      :quantity,
+      :unit_price,
+      :item_id,
+      :invoice_id,
+      :created_at,
+      :updated_at
+    )
+  end
+
+end
