9f816e56cb778392e529951de191d85ce3c8699b
John Travers <johnk.travers@gmail.com>
2019-11-24 11:47:11 -0700

    Add random transaction class method to transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -10,4 +10,8 @@ class Transaction < ApplicationRecord
   belongs_to :invoice
 
   scope :successful, -> { where(result: 0) }
+
+  def self.random_transaction
+    find(pluck(:id).sample)
+  end
 end
