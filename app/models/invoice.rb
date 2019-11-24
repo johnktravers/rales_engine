9212b5d73cf857fe45066e100f4f0e41d24c493c
John Travers <johnk.travers@gmail.com>
2019-11-23 21:01:06 -0700

    Add best_day class method to calculate an item's most sales date

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -17,4 +17,17 @@ class Invoice < ApplicationRecord
       .where('invoices.created_at::date = ? AND transactions.result = 0', date)
       .sum('invoice_items.quantity * invoice_items.unit_price')
   end
+
+  def self.best_day(item_id)
+    ActiveRecord::Base.connection.exec_query(
+      'SELECT invoices.created_at::date, sum(invoice_items.quantity)
+        FROM invoice_items
+        INNER JOIN invoices ON invoice_items.invoice_id = invoices.id
+        INNER JOIN transactions ON invoices.id = transactions.invoice_id
+        WHERE transactions.result = 0 AND invoice_items.item_id = ($1)
+        GROUP BY invoices.created_at::date
+        ORDER BY sum DESC, invoices.created_at::date DESC
+        LIMIT 1', 'item best day', [[nil, item_id]]
+    ).rows[0]
+  end
 end
