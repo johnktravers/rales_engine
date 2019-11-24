b2ceae5bee5f337b2267a6579740600388a8cffc
John Travers <johnk.travers@gmail.com>
2019-11-23 20:59:12 -0700

    Add test for top_items_by_revenue class method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -22,6 +22,18 @@ RSpec.describe Item, type: :model do
 
       expect(Item.random_item).to eq(items[0]).or eq(items[1]).or eq(items[2])
     end
+
+    it 'top items by revenue' do
+      @items = create_list(:item, 3)
+      @invoices = create_list(:invoice, 15)
+
+      @invoices.each_with_index do |invoice, i|
+        create(:invoice_item, invoice: invoice, unit_price: (i + 1) * 10 % 43, item: @items[i % 3])
+        create(:transaction, invoice: invoice, result: (i.even? ? 0 : 1))
+      end
+
+      expect(Item.top_items_by_revenue(2)).to eq([@items[2], @items[0]])
+    end
   end
 
 end
