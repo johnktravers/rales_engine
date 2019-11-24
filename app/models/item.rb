332636795207fa1f1e86706bbd4948021285e0d1
John Travers <johnk.travers@gmail.com>
2019-11-23 20:58:43 -0700

    Add relationship that an item has many transactions

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,8 @@ class Item < ApplicationRecord
 
   belongs_to :merchant
   has_many :invoice_items
-  has_many :invoices, through: :invoice_items
+  has_many :invoices,     through: :invoice_items
+  has_many :transactions, through: :invoices
 
   def self.random_item
     find(pluck(:id).sample)
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,6 +13,9 @@ RSpec.describe Item, type: :model do
     it { should belong_to :merchant }
     it { should have_many :invoice_items }
     it { should have_many(:invoices).through(:invoice_items) }
+    it { should have_many(:transactions).through(:invoices) }
+  end
+
   describe 'class methods' do
     it 'random item' do
       items = create_list(:item, 3)
