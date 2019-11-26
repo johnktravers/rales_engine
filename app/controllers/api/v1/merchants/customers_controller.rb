f291dbf5f6b29386d9843af9139cf85b70a80170
John Travers <johnk.travers@gmail.com>
2019-11-26 15:47:38 -0700

    Add index action to merchants customers controller for pending invoices

--- app/controllers/api/v1/merchants/customers_controller.rb
+++ app/controllers/api/v1/merchants/customers_controller.rb
@@ -2,10 +2,23 @@ class Api::V1::Merchants::CustomersController < ApplicationController
 
   def show
     merchant = Merchant.find_by(id: params[:id])
+
     if merchant
       customer = merchant.favorite_customer
       serialized_customer = CustomerSerializer.new(customer)
       render json: serialized_customer
+    else
+      render_show_error('Merchant')
+    end
+  end
+
+  def index
+    merchant = Merchant.find_by(id: params[:id])
+
+    if merchant
+      customers = Customer.order(:id).customers_with_pending_invoices(merchant.id)
+      serialized_customers = CustomerSerializer.new(customers)
+      render json: serialized_customers
     else
       render_show_error('Merchant')
     end
