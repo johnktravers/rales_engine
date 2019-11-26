87c0651beea8c9771c3a9553f61c67c3a978e424
John Travers <johnk.travers@gmail.com>
2019-11-26 15:46:00 -0700

    Add customers with pending invoices class method to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -19,4 +19,17 @@ class Customer < ApplicationRecord
       .order('count DESC')
       .first
   end
+
+  def self.customers_with_pending_invoices(merchant_id)
+    find_by_sql(
+      'SELECT customers.*
+        FROM customers
+        INNER JOIN invoices ON invoices.customer_id = customers.id
+        LEFT OUTER JOIN transactions ON invoices.id = transactions.invoice_id
+        WHERE invoices.merchant_id = ($1)
+        GROUP BY customers.id, invoices.id
+        HAVING count(transactions.id) = sum(transactions.result)
+          OR count(transactions.id) = 0', [[nil, merchant_id]]
+    )
+  end
 end
