3485d672fd218f70ec2d5e4f785fe176280c862f
John Travers <johnk.travers@gmail.com>
2019-11-27 11:27:59 -0700

    Add tests for top merchants by items sold and merchant revenue endpoints

--- spec/requests/api/v1/merchants/business_intel_request_spec.rb
+++ spec/requests/api/v1/merchants/business_intel_request_spec.rb
@@ -7,12 +7,13 @@ RSpec.describe 'Merchants API - business intelligence endpoints' do
       @merchants = create_list(:merchant, 3)
       @customers = create_list(:customer, 4)
       @merchants.each_with_index do |merchant, i|
+        create(:item, merchant: merchant)
         create_list(:invoice, 2, merchant: merchant, customer: @customers[i], created_at: "2012-03-#{i}4 15:54:10 UTC")
         create_list(:invoice, 3, merchant: merchant, customer: @customers[i + 1], created_at: "2012-03-#{i}4 15:54:10 UTC")
       end
 
       Invoice.all.each_with_index do |invoice, i|
-        create(:invoice_item, invoice: invoice, unit_price: (i + 1) * 10 % 43)
+        create(:invoice_item, invoice: invoice, unit_price: (i + 1) * 10 % 43, item: invoice.merchant.items.first, quantity: i)
         create(:transaction, invoice: invoice, result: (i.even? ? 0 : 1))
       end
     end
@@ -25,6 +26,17 @@ RSpec.describe 'Merchants API - business intelligence endpoints' do
       merchants = JSON.parse(response.body)
 
       expect(merchants['data'][0]['id'].to_i).to eq(@merchants[0].id)
+      expect(merchants['data'][1]['id'].to_i).to eq(@merchants[1].id)
+    end
+
+    it 'shows the top merchants by items sold' do
+      get '/api/v1/merchants/most_items?quantity=2'
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body)
+
+      expect(merchants['data'][0]['id'].to_i).to eq(@merchants[2].id)
       expect(merchants['data'][1]['id'].to_i).to eq(@merchants[1].id)
     end
 
@@ -46,6 +58,26 @@ RSpec.describe 'Merchants API - business intelligence endpoints' do
       customer = JSON.parse(response.body)
 
       expect(customer['data']['id'].to_i).to eq(@customers[1].id)
+    end
+
+    it 'shows the total revenue for a given merchant' do
+      get "/api/v1/merchants/#{@merchants[0].id}/revenue"
+
+      expect(response).to be_successful
+
+      revenue = JSON.parse(response.body)
+
+      expect(revenue['data']['attributes']['revenue']).to eq('47.00')
+    end
+
+    it 'shows the total revenue for a given merchant on a given date' do
+      get "/api/v1/merchants/#{@merchants[0].id}/revenue?date=2012-03-14"
+
+      expect(response).to be_successful
+
+      revenue = JSON.parse(response.body)
+
+      expect(revenue['data']['attributes']['revenue']).to eq('25.00')
     end
   end
 
@@ -75,14 +107,18 @@ RSpec.describe 'Merchants API - business intelligence endpoints' do
 
   describe 'endpoint error handling' do
     it 'shows an error if quantity is not a valid integer' do
-      get '/api/v1/merchants/most_revenue?quantity=-4'
+      paths = ['most_revenue', 'most_items']
 
-      expect(response).to be_successful
+      paths.each do |path|
+        get "/api/v1/merchants/#{path}?quantity=-4"
 
-      merchants = JSON.parse(response.body)
+        expect(response).to be_successful
 
-      expect(merchants['errors'][0]['title'])
-        .to eq('Please input an integer quantity that is greater than 0.')
+        merchants = JSON.parse(response.body)
+
+        expect(merchants['errors'][0]['title'])
+          .to eq('Please input an integer quantity that is greater than 0.')
+      end
     end
 
     it 'shows an error if date is not valid' do
