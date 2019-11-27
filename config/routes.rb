f3fefa66a0f2d7ef36e466c0e6856a92fafbbb4b
John Travers <johnk.travers@gmail.com>
2019-11-27 09:50:26 -0700

    Add route to top items by quantity sold business intelligence endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,7 @@ Rails.application.routes.draw do
         get '/:id/invoice_items', to: 'invoice_items#index'
         get '/:id/merchant',      to: 'merchants#show'
         get '/most_revenue',      to: 'revenue#index'
+        get '/most_items',        to: 'sales#index'
         get '/:id/best_day',      to: 'dates#show'
       end
 
